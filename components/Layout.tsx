01bacf41545d0fabaaf40fc69ee0d04905736f83
naoki1117 <105569021+naoki1117@users.noreply.github.com>
2022-10-18 01:35:05 +0900

    init

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import React, { FC, ReactNode } from 'react'
+import Head from "next/head"
+import { BadgeCheckIcon } from "@heroicons/react/solid"
+import { Badge } from 'tabler-icons-react'
+
+type Props = {
+    children:ReactNode
+    title:string
+}
+
+export const Layout:FC<Props> = ({children,title}) => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center font-mono'>
+        <Head>{title}</Head>
+        <header></header>
+        <main className='flex w-screen flex-1 flex-col items-center justify-center'>
+            {children}
+        </main>
+        <footer className='flex h-12 w-full items-center justify-center border-t'>
+            <BadgeCheckIcon className='h-6 w-6 text-blue-500'/>
+        </footer>
+
+    </div>
+
+  )
+}
+
+
---
+++ components/Spinner.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+export const Spinner:FC = () => {
+  return (
+    <div className='my-5 h-8 animate-spin rounded-full border-2 border-pink-600 border-t-transparent'>
+      
+    </div>
+  )
+}
+
+
