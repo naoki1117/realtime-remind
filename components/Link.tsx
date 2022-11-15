7a02e1d5fb754f892d7c5e1440228ef936e1ddf0
naoki1117 <105569021+naoki1117@users.noreply.github.com>
2022-11-16 07:35:20 +0900

    v29

--- components/Link.tsx
+++ components/Link.tsx
@@ -3,12 +3,19 @@ import { Menu, Button, Text } from '@mantine/core';
 import {useQueryPosts} from "../hooks/useQueryPosts"
 import { useSubscribePosts } from '../hooks/useSubscribePosts'
 import { Search,Badges } from 'tabler-icons-react'
+import { useInView } from 'react-intersection-observer';
 export const Link = () => {
+  const { ref, inView } = useInView({
+    // オプション
+    rootMargin: '1500px', // ref要素が現れてから50px過ぎたら
+    triggerOnce: false, 
+  });
 const { data: posts } = useQueryPosts()
   useSubscribePosts()
   return (
-    <div className="fixed top-1 right-1">
-      <Menu shadow="md" width={200} >
+    <div className="fixed top-1 right-1" >
+      <Menu shadow="md" width={200}>
+        {inView && <div className='h-5 w-5 bg-white'></div>}
       <Menu.Target>
         <Button className='absolute right-1 top-1'>
         
